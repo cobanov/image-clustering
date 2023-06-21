64d0affa00fea0bd088833c67e506555a98056de
Mert Cobanov <mertcobanov@gmail.com>
2023-06-21 09:55:03 +0300

    Update utils

--- utils.py
+++ utils.py
@@ -4,75 +4,90 @@ from PIL import Image
 import torch
 import matplotlib.pyplot as plt
 from pathlib import Path
-from tqdm import tqdm 
+from tqdm import tqdm
+
 
 def read_images_from_directory(image_directory: str) -> list:
     """
-    > It takes a directory as input and returns a list of all the images in that directory
-    :param image_directory: The directory where the images are stored
-    :type image_directory: str
-    :return: A list of images
+    Reads all images from the given directory and returns a list of image paths.
+    :param image_directory: The directory where the images are stored.
+    :return: A list of image paths.
     """
-
-    list_of_images = list()
-    for ext in ("*.gif", "*.png", "*.jpg", "*.jpeg"):
-        list_of_images.extend(
-            glob.glob(os.path.join(image_directory, ext))
-        )  # ? Remove sorted if it is there
+    list_of_images = []
+    image_extensions = (".gif", ".png", ".jpg", ".jpeg")
+    for ext in image_extensions:
+        list_of_images.extend(glob.glob(os.path.join(image_directory, ext)))
     print(f"Images found: {len(list_of_images)}")
-
     return list_of_images
 
 
 def read_with_pil(list_of_images: list, resize=True) -> list:
     """
-    > Reads a list of images and returns a list of PIL images
-    :param list_of_images: list of image paths
-    :type list_of_images: list
-    :param resize: If True, resize the image to 512x512, defaults to False (optional)
-    :return: A list of PIL images
+    Reads a list of images using PIL and returns a list of PIL images.
+    :param list_of_images: List of image paths.
+    :param resize: If True, resize the image to 512x512. Defaults to True.
+    :return: A list of PIL images.
     """
-    print('Images are reading...')
-    pil_images = list()
+    print("Reading images...")
+    pil_images = []
     for img_path in tqdm(list_of_images):
         img = Image.open(img_path).convert("RGB")
-        if resize:  #! No hard code
+        if resize:
             img.thumbnail((512, 512))
         pil_images.append(img)
-    print('Image reading done!')
-
+    print("Image reading done!")
     return pil_images
 
 
 def save_embeddings(tensor, path="embeddings.pt"):
+    """
+    Saves the tensor embeddings to a file.
+    :param tensor: The tensor to be saved.
+    :param path: The path where the tensor should be saved. Defaults to "embeddings.pt".
+    """
     torch.save(tensor, path)
-    print(f'Embeddings are saved to {path}')
+    print(f"Embeddings are saved to {path}")
 
 
 def load_from_embeddings(embedding_path):
-    """A brief description."""
-
+    """
+    Loads embeddings from the specified path.
+    :param embedding_path: The path of the embeddings file.
+    :return: The loaded embeddings tensor.
+    """
     vec = torch.load(embedding_path)
-    print('Embeddings loaded from folder.')
+    print("Embeddings loaded from folder.")
     if vec.dim() > 2:
         vec = torch.squeeze(vec)
     return vec
 
 
 def create_image_grid(label_images, project_name, label_number):
-
-    for i in range(len(label_images)):
+    """
+    Creates a grid of images with labels and saves it to a file.
+    :param label_images: List of labeled images.
+    :param project_name: The name of the project.
+    :param label_number: The label number.
+    """
+    fig, axs = plt.subplots(3, 3)
+    for i, ax in enumerate(axs.flat):
         if i >= 9:
             break
-        image = label_images[i]
-        plt.subplot(3, 3, i + 1)
-        plt.imshow(image, cmap="gray", interpolation="none")
-        plt.title(f"Class: {label_number}")
-        plt.axis("off")
-        plt.savefig(f"./clusters/{project_name}/cluster_{label_number}.png")
+    image = label_images[i]
+    ax.imshow(image, cmap="gray", interpolation="none")
+    ax.set_title(f"Class: {label_number}")
+    ax.axis("off")
+    plt.savefig(f"./clusters/{project_name}/cluster_{label_number}.png")
+    plt.close(fig)
 
 
 def create_dir(directory_path):
-    if not Path(directory_path).is_dir():
-        Path(directory_path).mkdir(exist_ok=True)
-    return Path(directory_path).stem
+    """
+    Creates a directory if it doesn't exist.
+    :param directory_path: The path of the directory.
+    :return: The stem of the directory path.
+    """
+    directory = Path(directory_path)
+    if not directory.is_dir():
+        directory.mkdir(exist_ok=True)
+    return directory.stem
