cdb190886a7f29db89c5a7055a47110e734100ee
Mert Cobanov <mertcobanov@gmail.com>
2023-06-21 09:44:03 +0300

    Update default pca dim changed to 16

--- clustering.py
+++ clustering.py
@@ -3,17 +3,17 @@ from scipy.cluster.vq import kmeans2
 import numpy as np
 
 
-def calculate_pca(embeddings, dim=8):
-    print('Calculating PCA')
+def calculate_pca(embeddings, dim=16):
+    print("Calculating PCA")
     pca = PCA(n_components=dim)
     pca_embeddings = pca.fit_transform(embeddings.squeeze())
-    print('PCA calculating done!')
+    print("PCA calculating done!")
     return pca_embeddings
 
 
 def calculate_kmeans(embeddings, k):
-    print('KMeans processing...')
+    print("KMeans processing...")
     centroid, labels = kmeans2(data=embeddings, k=k, minit="points")
     counts = np.bincount(labels)
-    print('Kmeans done!')
+    print("Kmeans done!")
     return centroid, labels
